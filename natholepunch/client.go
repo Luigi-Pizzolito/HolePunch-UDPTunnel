630533657d5d0d50b5032d8a7feea387b46bd384
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2024-01-09 02:39:32 +0800

    holepunch server code v1.0

--- natholepunch/client.go
+++ natholepunch/client.go
@@ -1,1 +1,7 @@
 package natholepunch
+
+import ()
+
+//! both clients need to request connection for hole punch to work?? check in network test
+
+//!on disconnect client must send connection request with null-byte as remoteID
--- natholepunch/server.go
+++ natholepunch/server.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 	"encoding/json"
 	"fmt"
+	"errors"
 
 	"time"
 	"net"
@@ -41,6 +42,16 @@ func (s *HPServer) deleteClient(ID string) {
 	defer s.RUnlock()
 	delete(s.ClientList, ID)
 }
+// checkClient checks if client already in the map
+func (s *HPServer) checkClient(ID string) (ClientData, error) {
+	s.RLock()
+	defer s.RUnlock()
+	i, ok := s.ClientList[ID]
+	if ok {
+		return i, nil
+	}
+	return i, errors.New("no such client")
+}
 
 // start serving and reply to client requests
 func (s *HPServer) Serve(serverPort string) error {
@@ -49,14 +60,14 @@ func (s *HPServer) Serve(serverPort string) error {
 	// Get server local address bind
 	serverAddr, err := net.ResolveUDPAddr("udp", ":"+serverPort)
 	if err != nil {
-		s.l.Warn("Error", zap.Error(err))
+		s.l.Warn("Error resolving server UDP addr: "+err.Error())
 		return err
 	}
 
 	// Listen to incoming UDP connections
 	serverConn, err := net.ListenUDP("udp", serverAddr)
 	if err != nil {
-		s.l.Warn("Error", zap.Error(err))
+		s.l.Warn("Error listening to UDP connections: "+err.Error())
 		return err
 	}
 	defer serverConn.Close()
@@ -69,7 +80,7 @@ func (s *HPServer) Serve(serverPort string) error {
 		buf := make([]byte, 1024)
 		n, newClientAddr, err := serverConn.ReadFromUDP(buf)
 		if err != nil {
-			s.l.Warn("Error", zap.Error(err))
+			s.l.Warn("Error reading UDP packet from client request: "+err.Error())
 			continue
 		}
 
@@ -82,6 +93,12 @@ func (s *HPServer) Serve(serverPort string) error {
 
 			// parse JSON request from client
 			json.Unmarshal(buf[:n], &incomingRequest)
+
+			// Here there are 3 types of requests which can be accepted
+			// the request type is defined by the content of incomingRequest.RemoteID
+			// ---- RemoteID string: 	connection to other client hole-punch request
+			// ---- "" string: 			get online client listing from server (idle client)
+			// ---- "\x00" string:		disconnect idle client from server
 
 			// Client disconnect request
 			if incomingRequest.RemoteID == "\x00" {
@@ -110,7 +127,27 @@ func (s *HPServer) Serve(serverPort string) error {
 				if incomingRequest.RemoteID == "" {
 					// Client is idle and wants list of online clients from server
 
-					// Return list of available online clients
+					// Build response to client JSON
+					// create copy of client list and remove requester LocalID, can't connect to yourself!
+					clientList := make(map[string]ClientData, len(s.ClientList))
+					for k, v := range s.ClientList {
+						clientList[k] = v
+					}
+					delete(clientList, incomingRequest.LocalID)
+
+					// Convert to JSON
+					responseToClientListingJSON, err := json.Marshal(clientList)
+					if err != nil {
+						s.l.Warn("Error preparing client listing response JSON: "+err.Error())
+						return
+					}
+
+					// Send response listing to client
+					_, err = serverConn.WriteToUDP(responseToClientListingJSON, newClientAddr)
+					if err != nil {
+						s.l.Warn("Error sending client listing response JSON: "+err.Error())
+						return
+					}
 
 					s.l.Info(incomingRequest.LocalID+" fetched list of clients.")
 
@@ -118,14 +155,73 @@ func (s *HPServer) Serve(serverPort string) error {
 				} else {
 					// Client is requesting hole-punch addr of other client
 
-					// Check if other client is online
+					clientFromMap, err := s.checkClient(incomingRequest.RemoteID)
+					// if requested RemoteID is not available (nil), client is in the waiting state until the requested RemoteID becomes online
 
+					//! or if the LocalID is not the requested RemoteID (remote ID also needs to accept/initiate the connection, 2way)
+					//! removed here so that any client can connect to any client in idle state
 
-					// Print punch request to logs
+					if err != nil /*|| clientFromMap.RemoteID != incomingRequest.LocalID*/ {
+						s.l.Info(incomingRequest.LocalID+" is waiting for "+incomingRequest.RemoteID)
+						// exit handle function
+						return
+					}
+
+					// If we have made it here, then the requested RemoteID is online and we can reply with its UDP address for hole punching
+
+					// Build response to client JSON (goes to original client that requested hole-punch)
+					responseToClientFromMap := ConnectResponse{
+						RemoteIP:	clientFromMap.LocalIP,
+						RemotePort:	clientFromMap.LocalPort,
+					}
+					responseToClientFromMapJSON, err := json.Marshal(responseToClientFromMap)
+					if err != nil {
+						s.l.Warn("Error preparing client response JSON: "+err.Error())
+						return
+					}
+
+					// Build response to new client JSON (goes to destination client to perform NAT hole-punching)
+					responseToPunchedClient := ConnectResponse{
+						RemoteIP:	newClientIP,
+						RemotePort:	newClientPort,
+					}
+					responseToPunchedClientJSON, err := json.Marshal(responseToPunchedClient)
+					if err != nil {
+						s.l.Warn("Error preparing punched client response JSON: "+err.Error())
+						return
+					}
+
+					// Send response to original requester client
+					_, err = serverConn.WriteToUDP(responseToClientFromMapJSON, newClientAddr)
+					if err != nil {
+						s.l.Warn("Error sending client response JSON: "+err.Error())
+						return
+					}
+
+					// Get UDP addr of punched client
+					clientFromMapUDPAddr, _ := net.ResolveUDPAddr("udp", clientFromMap.LocalIP+":"+clientFromMap.LocalPort)
+					if err != nil {
+						s.l.Warn("Error resolving punched client UDP addr: "+err.Error())
+						return
+					}
+
+					// Send UDP response to punched client NAT to perform hole-punch
+					_, err = serverConn.WriteToUDP(responseToPunchedClientJSON, clientFromMapUDPAddr)
+					if err != nil {
+						s.l.Warn("Error sending punch client punching response: "+err.Error())
+						return
+					}
+
+					// At this point hole-punch is completed!
+
+					// Remove the requester client from the waitlist queue
+					// s.deleteClient(clientFromMap.LocalID)
+					s.deleteClient(incomingRequest.LocalID)
+
+					// Print succesfull punch request to logs
 					s.printPunch(incomingRequest.LocalID,incomingRequest.RemoteID)
-					s.l.Info("New client requests hole-punch: "+incomingRequest.LocalID+" -> "+incomingRequest.RemoteID)
-					
-					return
+					s.l.Info("Punched NAT hole: "+incomingRequest.LocalID+" -> "+incomingRequest.RemoteID)
+				
 				}
 			}
 		}()
