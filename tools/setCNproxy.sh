b8cf64dd3e0df04898dd595e07b59ac803b1ea2b
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2024-01-11 14:52:04 +0800

    updated LICENSE and cleaned unneeded files

--- tools/setCNproxy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export GOPROXY=https://goproxy.cn
-export GOSUMDB=sum.golang.org # Since the goproxy.cn already supports proxying checksum databases, so no need to build the sum.golang.google.cn
